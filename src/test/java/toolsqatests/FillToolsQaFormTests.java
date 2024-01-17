cfd9c4943f74de97649ab32bb1a8ae3d73ceeaa4
Veronika Golikova <VGolikova@alfabank.ru>
2024-01-17 10:28:24 +0300

    fix hw 3

--- src/test/java/toolsqatests/FillToolsQaFormTests.java
+++ src/test/java/toolsqatests/FillToolsQaFormTests.java
@@ -8,26 +8,26 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.open;
 
-public class fillToolsQaFormTests {
+public class FillToolsQaFormTests {
 
     @Test
     void fillToolsQaFormTest() {
         open("https://demoqa.com/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
-        $(byId("firstName")).setValue("Veronika");
-        $(byId("lastName")).setValue("Golikova");
-        $(byId("userEmail")).setValue("someEmail@mail.ru");
-        $("#gender-radio-2").sibling(0).click();
+        $("#firstName").setValue("Veronika");
+        $("#lastName").setValue("Golikova");
+        $("#userEmail").setValue("someEmail@mail.ru");
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("7909111111");
-        $(byId("dateOfBirthInput")).click();
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1990");
         $(".react-datepicker__day--007").click();
-        $(byId("subjectsInput")).setValue("math").pressEnter();
-        $("#hobbies-checkbox-2").sibling(0).click();
+        $("#subjectsInput").setValue("math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("1.png");
-        $(byId("currentAddress")).setValue("Чертановская 41");
+        $("#currentAddress").setValue("Чертановская 41");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
@@ -36,8 +36,8 @@ public class fillToolsQaFormTests {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Veronika"), text("Golikova"),
-                text("someEmail@mail.ru"), text("7909111111"), text("Female"), text("07 July,1990"),
-                text("Maths"), text("Reading"), text("Чертановская 41"), text("NCR Noida"));
+        $(".table-responsive").shouldHave(text("Veronika"), text("Golikova"), text("someEmail@mail.ru"), text("7909111111"));
+        $(".table-responsive").shouldHave(text("Female"), text("07 July,1990"), text("Maths"));
+        $(".table-responsive").shouldHave(text("Reading"), text("Чертановская 41"), text("NCR Noida"));
     }
 }
