7570794438daf7e5e156a1afa1a9314e5b4af257
Veronika Golikova <VGolikova@alfabank.ru>
2024-02-24 12:30:12 +0300

    Add remoteUrl parameter

--- src/test/java/test/remotewebtests/TestBase.java
+++ src/test/java/test/remotewebtests/TestBase.java
@@ -18,15 +18,16 @@ public class TestBase {
         String browser = System.getProperty("browser", "chrome");
         String browserVersion = System.getProperty("browser_version", "100.0");
         String browserSize = System.getProperty("browser_size", "1920x1080");
-        String browserConfig = String.format("Browser: %s, version: %s, window size: %s",
-                browser, browserVersion, browserSize);
+        String remoteUrl = System.getProperty("remoteUrl", "https://user1:1111@selenoid.autotests.cloud/wd/hub");
+        String browserConfig = String.format("Browser: %s, version: %s, window size: %s, remoteUrl: %s",
+                browser, browserVersion, browserSize, remoteUrl);
         System.out.println(browserConfig);
         Configuration.pageLoadStrategy = "eager";
         Configuration.browser = browser;
         Configuration.browserVersion = browserVersion;
         Configuration.browserSize = browserSize;
         Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = remoteUrl;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
